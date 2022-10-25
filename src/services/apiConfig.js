f593e9fcabc676d1896958a6ae2e6400375e5b13
hoanggia <hoanggia1224@gmail.com>
2022-10-25 13:46:06 +0700

    Change url request

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -4,13 +4,13 @@ axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 // Get all posts
 export const getAllPosts = async () => {
-  const { data } = await axios.get('http://localhost:3000/blogs')
+  const { data } = await axios.get('http://20.194.171.46:3000/blogs')
   return await data
 }
 
 // Get post detail
 export const getPostDetails = async (id) => {
-  const { data } = await axios.get(`http://localhost:3000/blogs/${id}`)
+  const { data } = await axios.get(`http://20.194.171.46:3000/blogs/${id}`)
   return await data
 }
 
@@ -20,6 +20,6 @@ export const getPostDetails = async (id) => {
 
 // Delete a post
 export const deleteAPost = async (id) => {
-  const { data } = await axios.delete(`http://localhost:3000/blogs/${id}`)
+  const { data } = await axios.delete(`http://20.194.171.46:3000/blogs/${id}`)
   return await data
 }
